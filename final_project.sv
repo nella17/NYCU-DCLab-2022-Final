a92064bf741bc51791b1d5cdb80f5c861be293a0
Allen Guan <me@nella17.tw>
2022-12-19 20:07:31 +0800

    lab10 -> display

--- final_project.sv
+++ final_project.sv
@@ -28,15 +28,7 @@ module final_project import enum_type::*;
 
   // General VGA control signals
   wire clk_50MHz;       // 50MHz clock for VGA control
-  wire visible;         // when visible is 0, the VGA controller is sending
-                        // synchronization signals to the display device.
-  wire p_tick;          // when p_tick is 1, we must update the RGB value
-                        // based for the new coordinate (pixel_x, pixel_y)
-  wire [9:0] pixel_x;   // x coordinate of the next pixel (between 0 ~ 639) 
-  wire [9:0] pixel_y;   // y coordinate of the next pixel (between 0 ~ 479)
 
-  reg [3:0] tetris_x;
-  reg [4:0] tetris_y;
   reg inside_tetris;
   state_type tetris_ctrl, tetris_state;
   wire [9:0] tetris_bar_mask = 10'b1110111111;
@@ -44,12 +36,6 @@ module final_project import enum_type::*;
   wire [2:0] tetris_kind;
   wire [2:0] tetris_hold;
   wire [2:0] tetris_next [0:3];
-
-  /*clk_divider#(2) clk_divider0(
-    .clk(clk),
-    .reset(~reset_n),
-    .clk_out(clk_50MHz)
-  );*/
 
   clk_wiz_0 clk_wiz_0_0(
     .clk_50MHz(clk_50MHz),
@@ -80,37 +66,17 @@ module final_project import enum_type::*;
     .next(tetris_next)
   );
 
-  vga_sync_reg vs0(
-    .clk(clk_50MHz), .reset(~reset_n), .oHS(VGA_HSYNC), .oVS(VGA_VSYNC),
-    .visible(visible), .p_tick(p_tick),
-    .pixel_x(pixel_x), .pixel_y(pixel_y)
+  display display0(
+    .clk(clk_50MHz),
+    .reset_n(reset_n),
+    .VGA_HSYNC(VGA_HSYNC),
+    .VGA_VSYNC(VGA_VSYNC),
+    .VGA_RED(VGA_RED),
+    .VGA_GREEN(VGA_GREEN),
+    .VGA_BLUE(VGA_BLUE)
   );
 
   assign usr_led = usr_btn;
-
-  always @(posedge clk_50MHz) begin
-    tetris_x <= (pixel_x - 220) / 20;
-    tetris_y <= (pixel_y - 40) / 20;
-    inside_tetris <= (220 <= pixel_x) & (pixel_x < 420) & (40 <= pixel_y) & (pixel_y < 440);
-  end
-
-  always @(posedge clk_50MHz) begin
-    if (p_tick) begin
-      if (visible & inside_tetris) begin
-        case (tetris_kind)
-          3'b000: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'h000;
-          3'b001: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'h09D;
-          3'b010: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'h04F;
-          3'b011: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'hD90;
-          3'b100: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'hFF0;
-          3'b101: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'h0F3;
-          3'b110: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'h80C;
-          3'b111: {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'hF00;
-        endcase
-      end
-      else {VGA_RED, VGA_GREEN, VGA_BLUE} <= 12'h111;
-    end
-  end
 
   localparam row_init = "????????????????";
 
